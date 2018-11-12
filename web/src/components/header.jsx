733600327dfc0bdabdf91b57048257559f7dc72c
asd741 <a22833593@gmail.com>
2018-11-12 21:04:48 +0800

    build完成

--- web/src/components/header.jsx
+++ web/src/components/header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1 id="mainTitle">歡迎光臨彌留之地官方網站~</h1>
+                <h1 id="mainTitle">歡迎光臨彌留之地官方網站~~~~~</h1>
             </div>
         );
     }
