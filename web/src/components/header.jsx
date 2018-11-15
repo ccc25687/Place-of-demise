20efeba3d6bd0009540aa324a2617bd22a74da3a
asd741 <a22833593@gmail.com>
2018-11-15 14:11:44 +0800

    筆電版本更新

--- web/src/components/header.jsx
+++ web/src/components/header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <section className="header">
-                <video className="banner-video" src="../src/static/ball.mp4" type="video/mp4" autoPlay muted/>
+                <video className="banner-video" src="../src/static/ball.mp4" type="video/mp4" autoPlay muted loop/>
                 {/* <div className="go-bottom-button">往下</div> */}
             </section>
         );
